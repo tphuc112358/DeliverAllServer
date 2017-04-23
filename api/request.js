8aabe861812f01c3689e2f0d16b146a67d582656
tphuc112358 <tphuc112358@gmail.com>
2017-04-23 16:54:33 +0300

    disalbe new request

--- api/request.js
+++ api/request.js
@@ -1,4 +1,4 @@
-var sqlite3 = require('sqlite3').verbose();
+/*var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('NorDb.db');
 
 module.exports = (function() {
@@ -7,8 +7,27 @@ module.exports = (function() {
 	var b=["title","description","address","deli_address","deli_time","payment"];
 
 	router.post('/api/rest/login', function (req,res) {
+		var ret = (validate_request_param_auth(req.body));
 
+		if(ret.length > 0)
+		{
+			res.json({result:false,missing:ret});			
+		}
+
+		var col_db="";
+		var val_db="";
+		b.forEach(function(token) {
+			col_db+=token+",";
+			val_db+=req.body[token]+"\",\"";
+		});
+
+		col_db = col_db.slice(0,-1);
+		val_db = val_db.slice(0,-3);
+
+		//add into db
+		db.run("INSERT into User (" +col_db +") VALUES (\""+val_db+"\")");
+		res.json({result:"succesful"});
 	});
 
 	return router;
-}();
+}();*/
